1f0a1d0d339ffe233ae6fb4c70b34ea7931b8713
Eugene Wekesa <158033741+vsfdhyibavds@users.noreply.github.com>
2024-05-09 18:02:28 +0300

    Update control_flow.dart

--- control_flow.dart
+++ control_flow.dart
@@ -1,55 +1,55 @@
 // if and if elsestatement
-//void main() {
-//var age = 16;
-//if (age > 18) {
-//print("Genco is a voter in Kenya");
-//} else {
-//print("Genco is not a voter"); // else statement
-//}
-//}
+void main() {
+var age = 16;
+if (age > 18) {
+print("Genco is a voter in Kenya");
+} else {
+print("Genco is not a voter"); // else statement
+}
+}
 
 //switch case statement
-//void main() {
-//  int i = 11;
-//  switch (i) {
-//  case 1:
-//  print("value is 1");
-//  break;
-//  case 2:
-// print("value is 2");
-//  break;
-//  case 11:
-//  print("value is 11");
-// break;
-// default:
-//  print("out of range");
-//}
-//}
+void main() {
+  int i = 11;
+  switch (i) {
+  case 1:
+  print("value is 1");
+  break;
+  case 2:
+ print("value is 2");
+  break;
+  case 11:
+  print("value is 11");
+ break;
+ default:
+  print("out of range");
+}
+}
 
 // for loop
-//void main() {
-//  int num = 1;
-//  for (num; num <= 10; num++) //for loop to print 1-10
-//  {
-//    print(num);
-//  }
-//}
+void main() {
+  int num = 1;
+  for (num; num <= 10; num++) //for loop to print 1-10
+  {
+    print(num);
+  }
 
-//void main() {
-//  var list1 = [10, 20, 30, 40, 50];
-//  for (var i in list1) {
-//    print(i);
-//  }
-//}
 
-//void main() {
-//  var a = 1;
-//  var maxnum = 11;
-//  while (a < maxnum) {
-//    print(a);
-//    a = a + 1;
-//  }
-//}
+void main() {
+  var list1 = [10, 20, 30, 40, 50];
+  for (var i in list1) {
+    print(i);
+  }
+}
+
+void main() {
+  var a = 1;
+  var maxnum = 11;
+  while (a < maxnum) {
+    print(a);
+    a = a + 1;
+  }
+}
 
 //program that checks if number is odd,even or prime
 import 'dart:io';
